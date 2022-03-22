0d3c7185f53e33a90c459371e0585f13f3b5708b
abel <abelajetunmobi@gamil.com>
2022-03-22 15:43:14 +0100

    Add comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from './app';
 
 // listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // the functional port is 3000
 app.listen(PORT, () => {
   console.log('Server is listening on port 3000');
 });
