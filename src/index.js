4e58dd9c86ecf2bc26d573d6c1d563813e1a8353
Akinlua Bolamigbe <41027615+alob-mtc@users.noreply.github.com>
2022-03-22 13:30:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from './app';
 
 // listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log('Server is listening on port 3000');
 });
